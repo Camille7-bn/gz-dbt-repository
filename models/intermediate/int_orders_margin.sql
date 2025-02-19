35cd1003fd539a1f9966c5378d3615dc541eb5a7
Camille Boungnalith <camillebnith@gmail.com>
2025-02-19 10:44:51 +0000

    finance_days

---
+++ models/intermediate/int_orders_margin.sql
@@ -0,0 +1,23 @@
+{{ config(materialized='view') }}
+
+WITH sales_margin AS (
+    SELECT *
+    FROM {{ ref('int_sales_margin') }}
+),
+
+agg AS (
+    SELECT
+        orders_id,
+        date_date,
+        SUM(revenue) AS revenue,
+        SUM(quantity) AS quantity,
+        SUM(purchase_cost) AS purchase_cost,
+        SUM(margin) AS margin
+    FROM sales_margin
+    GROUP BY
+        orders_id,
+        date_date
+)
+
+SELECT *
+FROM agg
